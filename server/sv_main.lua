c43a47f686922a656c42433acb53f43b186a87bf
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-03-20 21:09:04 +0100

    Updated Repo Link

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -13,7 +13,7 @@ AddEventHandler("onResourceStart", function()
     if ph.check_for_updates == true then
       local currentVersion = ph.version
   
-      PerformHttpRequest("https://api.github.com/repos/Gerrxt07/playersystem/releases/latest", function(responseCode, resultData, resultHeaders)
+      PerformHttpRequest("https://api.github.com/repos/Gerrxt07/ph_playersystem/releases/latest", function(responseCode, resultData, resultHeaders)
         if responseCode == 200 then
           local releaseData = json.decode(resultData)
           local latestVersion = releaseData.tag_name

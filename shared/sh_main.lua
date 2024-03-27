f40bce7f7ae28d557da31c3db3aebfee634fa05c
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-03-27 20:17:05 +0100

    Updated descriptions

--- shared/sh_main.lua
+++ shared/sh_main.lua
@@ -2,7 +2,7 @@ ph = {}
 ph.version = "alpha-0.0.1"
 
 ph.check_for_updates = true -- Set to true by default. If true, you will receive a notification in the console that there is a new update.
-ph.maintenance = false -- Set to false by default. If set to true, only database registered admins can join. Other non-admin players will be kicked in-game if necessary. You can change the mode at any time with /maintenance (until restart).
+ph.maintenance = false -- Set to false by default. If set to true, only database registered admins can join. Other non-admin players will be kicked in-game if necessary.
 ph.language = "en" -- Set to "en" by default. Available languages: en (English), de (German), es (Spanish), fr (French), ru (Russian).
 ph.logging = true -- Set to true by default. If set to true, all actions will be logged in the console.
 

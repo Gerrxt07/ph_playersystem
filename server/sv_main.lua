baad208e647dbd82d099b96557dda12b52bff1b7
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-04-06 20:17:01 +0200

    test

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -85,7 +85,7 @@ AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
           end
         else -- Wenn Spieler nicht gefunden wurde
             deferrals.done(Locales[ph.language]['player_notwhitelisted'])
-            print("^1[PH]: " .. GetPlayerName(source) .. Locales[ph.language]['player_triedjoin'] .. " (IP: " .. playerIP .. ")")
+            print("^1[PH]: " .. GetPlayerName(source) .. Locales[ph.language]['player_triedjoin'])
         end
     end)
     end
